17f9fa8c40a610493a8435ce5c80572a7bfe8249
jeongyun <jeongyunw@gmail.com>
2020-01-11 18:09:28 +1300

    Update InsulationPagePreview.js

--- src/cms/preview-templates/InsulationPagePreview.js
+++ src/cms/preview-templates/InsulationPagePreview.js
@@ -5,12 +5,15 @@ import { InsulationPageTemplate } from "../../templates/insulation-page";
 const InsulationPagePreview = ({ entry, getAsset }) => {
   const data = entry.getIn(["data"]).toJS();
 
+  const entrySectionDetails = entry.getIn(["data", "section", "sectionDetail"]);
+  const sectionDetails = entrySectionDetails ? entrySectionDetails.toJS() : [];
+
   if (data) {
     return (
       <InsulationPageTemplate
         title={data.title}
         subTitle={data.subTitle}
-        section={data.section}
+        section={{ sectionDetails }}
       />
     );
   } else {
