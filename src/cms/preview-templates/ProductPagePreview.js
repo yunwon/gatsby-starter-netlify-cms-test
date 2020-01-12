011bc89e6792862842222c1dd0e9c661c65a43cb
jeongyun <jeongyunw@gmail.com>
2020-01-12 14:58:42 +1300

    Update ProductPagePreview.js

--- src/cms/preview-templates/ProductPagePreview.js
+++ src/cms/preview-templates/ProductPagePreview.js
@@ -1,56 +1,62 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { ProductPageTemplate } from '../../templates/product-page'
+import React from "react";
+import PropTypes from "prop-types";
+import { ProductPageTemplate } from "../../templates/product-page";
 
 const ProductPagePreview = ({ entry, getAsset }) => {
-  const entryBlurbs = entry.getIn(['data', 'intro', 'blurbs'])
-  const blurbs = entryBlurbs ? entryBlurbs.toJS() : []
+  const entryBlurbs = entry.getIn(["data", "intro", "blurbs"]);
+  const blurbs = entryBlurbs ? entryBlurbs.toJS() : [];
 
-  const entryTestimonials = entry.getIn(['data', 'testimonials'])
-  const testimonials = entryTestimonials ? entryTestimonials.toJS() : []
+  const entryTestimonials = entry.getIn(["data", "testimonials"]);
+  const testimonials = entryTestimonials ? entryTestimonials.toJS() : [];
 
-  const entryPricingPlans = entry.getIn(['data', 'pricing', 'plans'])
-  const pricingPlans = entryPricingPlans ? entryPricingPlans.toJS() : []
+  const entryPricingPlans = entry.getIn(["data", "pricing", "plans"]);
+  const pricingPlans = entryPricingPlans ? entryPricingPlans.toJS() : [];
 
   return (
     <ProductPageTemplate
-      image={entry.getIn(['data', 'image'])}
-      title={entry.getIn(['data', 'title'])}
-      heading={entry.getIn(['data', 'heading'])}
-      description={entry.getIn(['data', 'description'])}
-      intro={{ blurbs }}
-      main={{
-        heading: entry.getIn(['data', 'main', 'heading']),
-        description: entry.getIn(['data', 'main', 'description']),
-        image1: {
-          image: getAsset(entry.getIn(['data', 'main', 'image1', 'image'])),
-          alt: entry.getIn(['data', 'main', 'image1', 'alt']),
-        },
-        image2: {
-          image: getAsset(entry.getIn(['data', 'main', 'image2', 'image'])),
-          alt: entry.getIn(['data', 'main', 'image2', 'alt']),
-        },
-        image3: {
-          image: getAsset(entry.getIn(['data', 'main', 'image3', 'image'])),
-          alt: entry.getIn(['data', 'main', 'image3', 'alt']),
-        },
-      }}
-      fullImage={entry.getIn(['data', 'full_image'])}
-      testimonials={testimonials}
-      pricing={{
-        heading: entry.getIn(['data', 'pricing', 'heading']),
-        description: entry.getIn(['data', 'pricing', 'description']),
-        plans: pricingPlans,
+      image={entry.getIn(["data", "image"])}
+      title={entry.getIn(["data", "title"])}
+      id={entry.getIn(["data", "id"])}
+      intro={entry.getIn(["data", "intro"])}
+      productList={{
+        title: entry.getIn(["data", "productList", "title"]),
+        image: getAsset(entry.getIn(["data", "productList", "image"])),
+        id: entry.getIn(["data", "productList", "id"]),
+        intro: entry.getIn(["data", "productList", "intro"]),
+        descriptionList: {
+          title: entry.getIn([
+            "data",
+            "productList",
+            "descriptionList",
+            "title"
+          ]),
+          description: {
+            subTitle: entry.getIn([
+              "data",
+              "productList",
+              "descriptionList",
+              "title",
+              "subTitle"
+            ]),
+            subDescription: entry.getIn([
+              "data",
+              "productList",
+              "descriptionList",
+              "title",
+              "subDescription"
+            ])
+          }
+        }
       }}
     />
-  )
-}
+  );
+};
 
 ProductPagePreview.propTypes = {
   entry: PropTypes.shape({
-    getIn: PropTypes.func,
+    getIn: PropTypes.func
   }),
-  getAsset: PropTypes.func,
-}
+  getAsset: PropTypes.func
+};
 
-export default ProductPagePreview
+export default ProductPagePreview;
