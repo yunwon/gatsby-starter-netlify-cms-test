ca10a1a52b7784ff9e46b7abe86f8fc9a90c626f
jeongyun <jeongyunw@gmail.com>
2020-01-19 22:08:27 +1300

    Update healthyhomes-page-cn.js

--- src/templates/healthyhomes-page-cn.js
+++ src/templates/healthyhomes-page-cn.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import LayoutCN from "../layout/LayoutCN";
-import HealthyHomesPageTemplate from "./components/Services/healthyhomes";
+import HealthyHomesPageTemplate from "./components/Services/HealthyHomes";
 
 const HealthyHomesCNPage = ({ data }) => {
   const { frontmatter } = data.markdownRemark;
