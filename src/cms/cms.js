ddc33a88edb11f67014f2c0e288ca0f66d33cb59
jeongyun <jeongyunw@gmail.com>
2020-01-27 21:26:53 +1300

    Update cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,8 @@
 import CMS from "netlify-cms-app";
+
+import "typeface-poppins";
+import "../stylesheets/index.scss";
+
 import uploadcare from "netlify-cms-media-library-uploadcare";
 import cloudinary from "netlify-cms-media-library-cloudinary";
 
@@ -16,6 +20,28 @@ import ContactPagePreview from "./preview-templates/ContactPagePreview";
 import ContactCNPagePreview from "./preview-templates/ContactCNPagePreview";
 import HealthyHomesPagePreview from "./preview-templates/HealthyHomesPagePreview";
 import HealthyHomesCNPagePreview from "./preview-templates/HealthyHomesCNPagePreview";
+
+// The following window and global config settings below were taken from here.
+// https://github.com/gatsbyjs/gatsby/blob/master/docs/docs/visual-testing-with-storybook.md
+// They're required because the netlify-cms runs on a separate webpack config,
+// and outside of Gatsby. This ensures any Gatsby components imported into the
+// CMS works without errors
+
+// highlight-start
+// Gatsby's Link overrides:
+// Gatsby defines a global called ___loader to prevent its method calls from creating console errors you override it here
+global.___loader = {
+  enqueue: () => {},
+  hovering: () => {}
+};
+
+// Gatsby internal mocking to prevent unnecessary errors
+global.__PATH_PREFIX__ = "";
+
+// This is to utilized to override the window.___navigate method Gatsby defines and uses to report what path a Link would be taking us to
+window.___navigate = pathname => {
+  alert(`This would navigate to: https://www.memsql.com${pathname}`);
+};
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
