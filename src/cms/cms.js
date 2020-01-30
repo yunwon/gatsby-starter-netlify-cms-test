36df82169646aee07378e3b85eeb063ec86db532
jeongyun <jeongyunw@gmail.com>
2020-01-30 16:46:46 +1300

    Update cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,6 +2,7 @@ import CMS from "netlify-cms-app";
 
 import "typeface-poppins";
 import "../stylesheets/index.scss";
+import "../templates/components/Index/index-page.module.scss";
 
 import uploadcare from "netlify-cms-media-library-uploadcare";
 import cloudinary from "netlify-cms-media-library-cloudinary";
