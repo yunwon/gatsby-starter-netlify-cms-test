75da93bcf5d00e9ff5ad255031b81d477186b5ee
jeongyun <jeongyunw@gmail.com>
2020-02-12 21:26:45 +1300

    updated contact page preview

--- src/cms/preview-templates/ContactCNPagePreview.js
+++ src/cms/preview-templates/ContactCNPagePreview.js
@@ -2,15 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 import ContactPageTemplate from "../../templates/components/Contact/Contact";
 
-const ContactCNPagePreview = ({ entry, widgetFor }) => (
-  <ContactPageTemplate
-    title={entry.getIn(["data", "title"])}
-    subTitle={entry.getIn(["data", "subTitle"])}
-    description={entry.getIn(["data", "description"])}
-    form={entry.getIn(["data", "form"])}
-    submitSuccessMessage={entry.getIn(["data", "submitSuccessMessage"])}
-  />
-);
+const ContactCNPagePreview = ({ entry, widgetFor }) => {
+  const data = entry.getIn(["data"]).toJS();
+
+  if (data) {
+    return (
+      <ContactPageTemplate
+        title={data.title}
+        subTitle={data.subTitle}
+        description={data.description}
+        form={data.form}
+        submitSuccessMessage={data.submitSuccessMessage}
+      />
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
+};
 
 ContactCNPagePreview.propTypes = {
   entry: PropTypes.shape({
--- src/cms/preview-templates/ContactPagePreview.js
+++ src/cms/preview-templates/ContactPagePreview.js
@@ -2,15 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 import ContactPageTemplate from "../../templates/components/Contact/Contact";
 
-const ContactPagePreview = ({ entry, widgetFor }) => (
-  <ContactPageTemplate
-    title={entry.getIn(["data", "title"])}
-    subTitle={entry.getIn(["data", "subTitle"])}
-    description={entry.getIn(["data", "description"])}
-    form={entry.getIn(["data", "form"])}
-    submitSuccessMessage={entry.getIn(["data", "submitSuccessMessage"])}
-  />
-);
+const ContactPagePreview = ({ entry, widgetFor }) => {
+  const data = entry.getIn(["data"]).toJS();
+
+  if (data) {
+    return (
+      <ContactPageTemplate
+        title={data.title}
+        subTitle={data.subTitle}
+        description={data.description}
+        form={data.form}
+        submitSuccessMessage={data.submitSuccessMessage}
+      />
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
+};
 
 ContactPagePreview.propTypes = {
   entry: PropTypes.shape({
