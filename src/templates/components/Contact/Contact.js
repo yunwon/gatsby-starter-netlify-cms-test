c380d59613e95585953f35c703921fd0e84efc1e
jeongyun <jeongyunw@gmail.com>
2020-02-12 17:17:21 +1300

    form test

--- src/templates/components/Contact/Contact.js
+++ src/templates/components/Contact/Contact.js
@@ -61,87 +61,112 @@ class ContactPageTemplate extends React.Component {
           </div>
           <div className={styles.form}>
             {!this.state.submitted ? (
+              // <form
+              //   name="contact"
+              //   method="post"
+              //   data-netlify="true"
+              //   data-netlify-honeypot="bot-field"
+              //   onSubmit={this.handleSubmit}
+              // >
+              //   {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+              //   <input type="hidden" name="form-name" value="contact" />
+              //   <div hidden>
+              //     <label>
+              //       Don’t fill this out:{" "}
+              //       <input name="bot-field" onChange={this.handleChange} />
+              //     </label>
+              //   </div>
+              //   <div className={styles.field}>
+              //     <label htmlFor="name">{formData.yourName.name}</label>
+              //     <input
+              //       placeholder={formData.yourName.placeholder}
+              //       type="text"
+              //       name="name"
+              //       onChange={this.handleChange}
+              //       id="name"
+              //       required={true}
+              //     />
+              //   </div>
+              //   <div className={styles.field}>
+              //     <label htmlFor={"phone"}>{formData.phone.name}</label>
+              //     <input
+              //       placeholder={formData.phone.placeholder}
+              //       type={"tel"}
+              //       name={"phone"}
+              //       onChange={this.handleChange}
+              //       id={"phone"}
+              //       required={true}
+              //     />
+              //   </div>
+              //   <div className={styles.field}>
+              //     <label htmlFor={"email"}>{formData.email.name}</label>
+              //     <input
+              //       placeholder={formData.email.placeholder}
+              //       type={"email"}
+              //       name={"email"}
+              //       onChange={this.handleChange}
+              //       id={"email"}
+              //       required={true}
+              //     />
+              //   </div>
+              //   <div className={styles.field}>
+              //     <label htmlFor={"type"}>{formData.type.name}</label>
+              //     {/* <select name={"type"} id={"type"}>
+              //       {this.props.form.type.option.map(item => (
+              //         <option value={item.value}>{item.name}</option>
+              //       ))}
+              //     </select> */}
+              //     <Dropdown
+              //       options={formData.type.option}
+              //       onChange={this._onSelect}
+              //       placeholder="Select an option"
+              //       className="select"
+              //       name={"type"}
+              //       id={"type"}
+              //     />
+              //   </div>
+              //   <div className={styles.field}>
+              //     <label htmlFor={"message"}>{formData.message.name}</label>
+              //     <textarea
+              //       placeholder={formData.message.placeholder}
+              //       rows={7}
+              //       name={"message"}
+              //       onChange={this.handleChange}
+              //       id={"message"}
+              //       required={true}
+              //     />
+              //   </div>
+              //   <div className={styles.field}>
+              //     <div data-netify-recaptcha="true"></div>
+              //   </div>
+              //   <div className={styles.submitButton}>
+              //     <Button title={formData.submit} type="submit" />
+              //   </div>
+              // </form>
               <form
-                name="contact"
                 method="post"
                 data-netlify="true"
                 data-netlify-honeypot="bot-field"
                 onSubmit={this.handleSubmit}
               >
-                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-                <input type="hidden" name="form-name" value="contact" />
-                <div hidden>
-                  <label>
-                    Don’t fill this out:{" "}
-                    <input name="bot-field" onChange={this.handleChange} />
-                  </label>
-                </div>
-                <div className={styles.field}>
-                  <label htmlFor="name">{formData.yourName.name}</label>
-                  <input
-                    placeholder={formData.yourName.placeholder}
-                    type="text"
-                    name="name"
-                    onChange={this.handleChange}
-                    id="name"
-                    required={true}
-                  />
-                </div>
-                <div className={styles.field}>
-                  <label htmlFor={"phone"}>{formData.phone.name}</label>
-                  <input
-                    placeholder={formData.phone.placeholder}
-                    type={"tel"}
-                    name={"phone"}
-                    onChange={this.handleChange}
-                    id={"phone"}
-                    required={true}
-                  />
-                </div>
-                <div className={styles.field}>
-                  <label htmlFor={"email"}>{formData.email.name}</label>
-                  <input
-                    placeholder={formData.email.placeholder}
-                    type={"email"}
-                    name={"email"}
-                    onChange={this.handleChange}
-                    id={"email"}
-                    required={true}
-                  />
-                </div>
-                <div className={styles.field}>
-                  <label htmlFor={"type"}>{formData.type.name}</label>
-                  {/* <select name={"type"} id={"type"}>
-                    {this.props.form.type.option.map(item => (
-                      <option value={item.value}>{item.name}</option>
-                    ))}
-                  </select> */}
-                  <Dropdown
-                    options={formData.type.option}
-                    onChange={this._onSelect}
-                    placeholder="Select an option"
-                    className="select"
-                    name={"type"}
-                    id={"type"}
-                  />
-                </div>
-                <div className={styles.field}>
-                  <label htmlFor={"message"}>{formData.message.name}</label>
-                  <textarea
-                    placeholder={formData.message.placeholder}
-                    rows={7}
-                    name={"message"}
-                    onChange={this.handleChange}
-                    id={"message"}
-                    required={true}
-                  />
-                </div>
-                <div className={styles.field}>
-                  <div data-netify-recaptcha="true"></div>
-                </div>
-                <div className={styles.submitButton}>
-                  <Button title={formData.submit} type="submit" />
-                </div>
+                <label>
+                  Name
+                  <input type="text" name="name" id="name" />
+                </label>
+                <label>
+                  Email
+                  <input type="email" name="email" id="email" />
+                </label>
+                <label>
+                  Subject
+                  <input type="text" name="subject" id="subject" />
+                </label>
+                <label>
+                  Message
+                  <textarea name="message" id="message" rows="5" />
+                </label>
+                <button type="submit">Send</button>
+                <input type="reset" value="Clear" />
               </form>
             ) : (
               <div className={styles.thanks}>
